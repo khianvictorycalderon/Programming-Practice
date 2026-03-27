c0791cf9764238fbf1f49d8482a097c26fcf5f1c
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-27 22:12:01 +0800

    Updated javascript selection sorting

--- selection-sorting.js
+++ selection-sorting.js
@@ -5,23 +5,31 @@ const input = require("./input");
     const numbers = await input("Enter numbers (Separated by input): ");
     const num_list = numbers.replace(/\s/g, "").split(",").map(Number);
 
-    function geMin(arr) {
-        // Assume the first number is the highest:
-        let maxNum = arr[0];
-        arr.forEach(item => {
-            if (item < maxNum) maxNum = item;
-        });
-        return maxNum;
+    function selectionSort(arr) {
+
+        // Make original copy
+        const origCopy = [...arr];
+        const sortedArray = [];
+
+        function geMin(arr) {
+            // Assume the first number is the highest:
+            let maxNum = arr[0];
+            arr.forEach(item => {
+                if (item < maxNum) maxNum = item;
+            });
+            return maxNum;
+        }
+
+        while (origCopy.length > 0) {
+            const smallestNumber = geMin(origCopy);
+            sortedArray.push(smallestNumber);
+            const index = origCopy.indexOf(smallestNumber);
+            if (index > -1) origCopy.splice(index, 1);
+        }
+
+        return sortedArray;
     }
 
-    const sortedArray = [];
-    while (num_list.length > 0) {
-        const smallestNumber = geMin(num_list);
-        sortedArray.push(smallestNumber);
-        const index = num_list.indexOf(smallestNumber);
-        if (index > -1) num_list.splice(index, 1);
-    }
-
-    console.log(`The new updated orders of number are: ${sortedArray.join(", ")}`);
+    console.log(`The new updated orders of number are: ${selectionSort(num_list).join(", ")}`);
 
 })();
