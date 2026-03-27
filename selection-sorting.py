0b46cdbd3e3d1e69e7b35547be6ca03137dc8489
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-27 22:14:29 +0800

    Updated python selection sort

--- selection-sorting.py
+++ selection-sorting.py
@@ -1,18 +1,24 @@
 numbers = input("Enter numbers (Separated by input): ")
 num_list = [int(num) for num in numbers.split(",")]
 
-def getMin(lt):
-    # Assume the first number is the highest
-    min = lt[0]
-    for item in lt:
-        if item < min:
-            min = item
-    return min
+def selectionSort(arr):
 
-sorted_numbers = []
-while (len(num_list) > 0):
-    minNum = getMin(num_list)
-    sorted_numbers.append(minNum)
-    num_list.remove(minNum)
+    origCopy = arr.copy()
+    sorted_numbers = []
 
-print(f"The new updated orders of number are: {", ".join(map(str, sorted_numbers))}")
+    def getMin(lt):
+        # Assume the first number is the highest
+        min = lt[0]
+        for item in lt:
+            if item < min:
+                min = item
+        return min
+    
+    while (len(origCopy) > 0):
+        minNum = getMin(origCopy)
+        sorted_numbers.append(minNum)
+        origCopy.remove(minNum)
+
+    return sorted_numbers
+
+print(f"The new updated orders of number are: {", ".join(map(str, selectionSort(num_list)))}")
