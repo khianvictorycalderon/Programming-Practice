dc74b4b3bae8dd833ff114d52882b13887db5bfb
khianvictorycalderon <ckhianvictory@gmail.com>
2026-03-27 21:28:48 +0800

    Added javascript mean

---
+++ mean.js
@@ -0,0 +1,19 @@
+const input = require("./input");
+
+(async() => {
+    
+    const numbers = await input("Enter numbers (Separated by Comma): ");
+    const num_list = numbers.replace(" ", "").split(",");
+    const num_length = num_list.length;
+
+    let sum = 0;
+
+    // Total sum of all numbers
+    num_list.forEach(number => sum += Number(number));
+
+    // Get the average
+    const result = sum / num_length;
+
+    console.log(`The average of ${num_list.join(", ")} is ${result}`);
+
+})();
